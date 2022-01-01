22c0e9d2623cb44018f2022c221a6fa1d0b8f037
mogooee <mogoo22@naver.com>
2022-01-02 01:26:02 +0900

    feat: local storage

--- todo-list.js
+++ todo-list.js
@@ -6,42 +6,75 @@ const complete = "fa-check-square";
 class ToDo {
   constructor() {
     this.id = 0;
+    this.storage = [];
     this.init();
   }
 
   init() {
     this.InitEventListeners();
+    this.LoadLocalStorage();
   }
 
-  AddList() {
-    const newTask = $(".todo__input-text").value;
+  LoadLocalStorage() {
+    const loadToDos = localStorage.getItem("TODO");
+    if (loadToDos) {
+      const parsedToDos = JSON.parse(loadToDos);
+      parsedToDos.forEach((e) => {
+        if (!e.trash) {
+          this.Rendering(e.id, e.newTask, e.done);
+        }
+      });
+      this.storage = parsedToDos;
+      this.id = parsedToDos.length;
+    }
+  }
+
+  Rendering(id, newTask, done) {
     if (!newTask) return;
+
     const task = `<li>
-     <div class="todo__column"><i id="${this.id}" class="done far ${unComplete}"></i></div>
+     <div class="todo__column"><i id="${id}" class="done far ${
+      done ? complete : unComplete
+    }"></i></div>
      <div class="todo__column">${newTask}</div>
-     <div class="todo__column"><i id="${this.id}" class="remove fas fa-trash"></i></div>
+     <div class="todo__column"><i id="${id}" class="remove fas fa-trash"></i></div>
      </li> 
      `;
     const position = "beforeend";
     $(".todo__list").insertAdjacentHTML(position, task);
     $(".todo__input-text").value = "";
-    this.id++;
+  }
+
+  AddStorage(id, newTask) {
+    this.storage.push({ id, newTask, done: 0, trash: 0 });
   }
 
   DoneList(id) {
     const doneElement = document.getElementById(`${id}`);
     doneElement.classList.toggle(complete);
     doneElement.classList.toggle(unComplete);
+    this.storage[id].done = this.storage[id].done ? 0 : 1;
+    this.SaveLocalStorage();
   }
 
   DeleteList(id) {
     const li = document.getElementById(`${id}`).parentNode.parentNode;
     li.parentNode.removeChild(li);
+    this.storage[id].trash = 1;
+    this.SaveLocalStorage();
+  }
+
+  SaveLocalStorage() {
+    localStorage.setItem("TODO", JSON.stringify(this.storage));
   }
 
   InitEventListeners() {
     $(".todo__input-btn").addEventListener("click", (e) => {
-      this.AddList();
+      const inputValue = $(".todo__input-text").value;
+      this.Rendering(this.id, inputValue);
+      this.AddStorage(this.id, inputValue);
+      this.SaveLocalStorage();
+      this.id++;
     });
     $(".todo__input").addEventListener("submit", (e) => {
       e.preventDefault();
